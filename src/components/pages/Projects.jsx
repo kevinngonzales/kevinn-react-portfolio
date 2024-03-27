ebb119302cb364ca7481ffc478214d5e1f0f52e6
kevinngonzales <kevinngstem@gmail.com>
2024-03-26 22:46:28 -0700

    test

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 
-import GithubApiApp from "../PROJECTS/GithubApiApp.jsx";
+import GithubApiApp from "../projects/GithubApiApp.jsx";
 
-import MemeGenerator from "../PROJECTS/MemeGenerator.jsx";
+import MemeGenerator from "../projects/MemeGenerator.jsx";
 
-import ReactRefactor from "../PROJECTS/ReactRefactor.jsx";
+import ReactRefactor from "../projects/ReactRefactor.jsx";
 
-import Pokemon from "../PROJECTS/Pokemon.jsx";
+import Pokemon from "../projects/Pokemon.jsx";
 
-import TriviaWebApp from "../PROJECTS/TriviaWebApp.jsx";
+import TriviaWebApp from "../projects/TriviaWebApp.jsx";
 
 function Projects() {
   return (
